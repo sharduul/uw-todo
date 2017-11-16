badad9aa8ee1b73807d94cdea6b689f00e81e346
Shardul Bagade <dool@Sharduls-MacBook-Air.local>
2017-11-15 19:58:39 -0500

    setup tests

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,8 @@ export class A2gtmPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getBoardTitle(){
+    return element(by.css('.boards-wrapper h2')).getText();
+  }
 }
