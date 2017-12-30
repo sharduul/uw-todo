5f22bc7205246868db7595e398e870d5d471d809
Shardul Bagade <dool@Sharduls-MacBook-Air.local>
2017-12-30 13:04:19 -0500

    nightmare init

---
+++ test/index.js
@@ -0,0 +1,26 @@
+﻿var path = require('path');
+var Nightmare = require('nightmare');
+var should = require('chai').should();
+
+describe('Nightmare demo', function () {
+    this.timeout(15000); // Set timeout to 15 seconds, instead of the original 2 seconds
+
+    //var url = 'http://localhost:3000';
+    var url = 'http://localhost:4200/';
+
+    describe('Start page', function () {
+        it('should show form when loaded', function (done) {
+            new Nightmare()
+                .goto(url)
+                .evaluate(function () {
+                    return document.querySelectorAll('app-root h1').length;
+                }, function (result) {
+                    result.should.equal(1);
+                    done();
+                })
+                .run();
+        });
+    });
+
+   
+});
