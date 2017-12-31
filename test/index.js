75b3d903ede4417becfa9e7863f48620b73aa4d5
Shardul Bagade <dool@Sharduls-MacBook-Air.local>
2017-12-31 10:03:15 -0500

    nightmare add card

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 ﻿var path = require('path');
 var Nightmare = require('nightmare');
 var should = require('chai').should();
+var expect = require('chai').expect;
 
 describe('Nightmare demo', function () {
     this.timeout(15000); // Set timeout to 15 seconds, instead of the original 2 seconds
@@ -9,7 +10,7 @@ describe('Nightmare demo', function () {
     var url = 'http://localhost:4200/';
 
     describe('Start page', function () {
-        it('should show form when loaded', function (done) {
+        it('should show headline when loaded', function (done) {
             new Nightmare()
                 .goto(url)
                 .evaluate(function () {
@@ -19,6 +20,40 @@ describe('Nightmare demo', function () {
                     result.should.equal(1);
                     done();
                 });
+        });
+    });
+
+    describe('Board page', function () {
+        url = 'http://localhost:4200/b/5a0b830fe4468fef1806a6e2';
+
+        it('should add card to the board', function (done) {
+            var originalCount = 0;
+            var newCount = 0;
+            var nightmare = Nightmare({ show: false })
+
+            nightmare
+                .goto(url)
+                .wait('.card-list li.card')
+                .evaluate(function () {
+                    originalCount = document.querySelectorAll('.card-list li.card').length;
+                    return originalCount;
+                })
+                .then(function (originalCount) {
+                    nightmare
+                        .wait('div.add-card')
+                        .click('div.add-card')
+                        .wait('div.add-card .add-card-input')
+                        .type('div.add-card .add-card-input', 'github nightmare')
+                        .type('body', '\u000d') // enter key
+                        .evaluate(function () {
+                            newCount = document.querySelectorAll('.card-list li.card').length;
+                            return newCount;
+                        })
+                        .then(function (newCount) {
+                            expect(newCount).to.equal(originalCount + 1);
+                            done();
+                        })
+                })
         });
     });
 
