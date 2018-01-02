0117455041b27cbe18cd565846f7bf833b9b7455
SHARDUL\Shardul <bagadeshardul@gmail.com>
2018-01-02 15:49:29 -0500

    mark as done

--- test/index.js
+++ test/index.js
@@ -26,141 +26,141 @@ describe('Nightmare demo', function () {
     describe('Board page', function () {
         url = 'http://localhost:4200/b/5a0c571d6b5b743454fac770';
 
-        // it('should add card to the board', function (done) {
-        //     var originalCount = 0;
-        //     var newCount = 0;
-        //     var nightmare = Nightmare({ show: false })
+        it('should add card to the board', function (done) {
+            var originalCount = 0;
+            var newCount = 0;
+            var nightmare = Nightmare({ show: false })
 
-        //     nightmare
-        //         .goto(url)
-        //         .wait('.card-list li.card')
-        //         .evaluate(function () {
-        //             originalCount = document.querySelectorAll('.card-list li.card').length;
-        //             return originalCount;
-        //         })
-        //         .then(function (originalCount) {
-        //             nightmare
-        //                 .wait('div.add-card')
-        //                 .click('div.add-card')
-        //                 .wait('div.add-card .add-card-input')
-        //                 .type('div.add-card .add-card-input', 'github nightmare')
-        //                 .type('body', '\u000d') // enter key
-        //                 .evaluate(function () {
-        //                     newCount = document.querySelectorAll('.card-list li.card').length;
-        //                     return newCount;
-        //                 })
-        //                 .then(function (newCount) {
-        //                     expect(newCount).to.equal(originalCount + 1);
-        //                     done();
-        //                 })
-        //         })
-        // });
+            nightmare
+                .goto(url)
+                .wait('.card-list li.card')
+                .evaluate(function () {
+                    originalCount = document.querySelectorAll('.card-list li.card').length;
+                    return originalCount;
+                })
+                .then(function (originalCount) {
+                    nightmare
+                        .wait('div.add-card')
+                        .click('div.add-card')
+                        .wait('div.add-card .add-card-input')
+                        .type('div.add-card .add-card-input', 'github nightmare')
+                        .type('body', '\u000d') // enter key
+                        .evaluate(function () {
+                            newCount = document.querySelectorAll('.card-list li.card').length;
+                            return newCount;
+                        })
+                        .then(function (newCount) {
+                            expect(newCount).to.equal(originalCount + 1);
+                            done();
+                        })
+                })
+        });
 
-        // it('should delete card from the board', function (done) {
-        //     var originalCount = 0;
-        //     var newCount = 0;
-        //     var nightmare = Nightmare({ show: false })
+        it('should delete card from the board', function (done) {
+            var originalCount = 0;
+            var newCount = 0;
+            var nightmare = Nightmare({ show: false })
 
-        //     nightmare
-        //         .goto(url)
-        //         .wait('.card-list li.card')
-        //         .evaluate(function () {
-        //             originalCount = document.querySelectorAll('.card-list li.card').length;
-        //             return originalCount;
-        //         })
-        //         .then(function () {
-        //             nightmare
-        //                 .wait('div.card-name .card-delete-btn')
-        //                 .click('div.card-name .card-delete-btn')
-        //                 .evaluate(function () {
-        //                     newCount = document.querySelectorAll('.card-list li.card').length;
-        //                     return newCount;
-        //                 })
-        //                 .then(function () {
-        //                     expect(newCount).to.equal(0);
-        //                     done();
-        //                 })
-        //         })
-        // });
+            nightmare
+                .goto(url)
+                .wait('.card-list li.card')
+                .evaluate(function () {
+                    originalCount = document.querySelectorAll('.card-list li.card').length;
+                    return originalCount;
+                })
+                .then(function () {
+                    nightmare
+                        .wait('div.card-name .card-delete-btn')
+                        .click('div.card-name .card-delete-btn')
+                        .evaluate(function () {
+                            newCount = document.querySelectorAll('.card-list li.card').length;
+                            return newCount;
+                        })
+                        .then(function () {
+                            expect(newCount).to.equal(0);
+                            done();
+                        })
+                })
+        });
 
 
-        // it('should add description to card', function (done) {
-        //     var nightmare = Nightmare({ show: false })
+        it('should add description to card', function (done) {
+            var nightmare = Nightmare({ show: false })
 
-        //     nightmare
-        //         .goto(url)
-        //         .wait('.card-edit-btn')
-        //         .evaluate(()=>{
-        //             var elements = document.querySelectorAll('.card-edit-btn');
-        //             elements[0].click();
-        //         })
-        //         .wait('textarea.description')
-        //         .evaluate(function () {
-        //             var elements = document.querySelectorAll('textarea.description');
-        //             elements[0].value = ""; // clear description
-        //         })
-        //         .type('textarea.description', 'test')
-        //         .type('body', '\u000d') // enter key
-        //         .evaluate(()=>{
-        //             var elements = document.querySelectorAll('.card-edit-btn');
-        //             elements[0].click();
-        //         })
-        //         .then(function () {
-        //             nightmare
-        //                 .evaluate(()=>{
-        //                     var elements = document.querySelectorAll('.card-edit-btn');
-        //                     elements[0].click();
-        //                 })
-        //                 .wait('textarea.description')
-        //                 .evaluate(function () {
-        //                     var elements = document.querySelectorAll('textarea.description');
-        //                     return elements[0].value;
-        //                 })
-        //                 .then(function (value) {
-        //                     expect(value).to.equal("test");
-        //                     done();
-        //                 })
-        //         })
-        // });
+            nightmare
+                .goto(url)
+                .wait('.card-edit-btn')
+                .evaluate(()=>{
+                    var elements = document.querySelectorAll('.card-edit-btn');
+                    elements[0].click();
+                })
+                .wait('textarea.description')
+                .evaluate(function () {
+                    var elements = document.querySelectorAll('textarea.description');
+                    elements[0].value = ""; // clear description
+                })
+                .type('textarea.description', 'test')
+                .type('body', '\u000d') // enter key
+                .evaluate(()=>{
+                    var elements = document.querySelectorAll('.card-edit-btn');
+                    elements[0].click();
+                })
+                .then(function () {
+                    nightmare
+                        .evaluate(()=>{
+                            var elements = document.querySelectorAll('.card-edit-btn');
+                            elements[0].click();
+                        })
+                        .wait('textarea.description')
+                        .evaluate(function () {
+                            var elements = document.querySelectorAll('textarea.description');
+                            return elements[0].value;
+                        })
+                        .then(function (value) {
+                            expect(value).to.equal("test");
+                            done();
+                        })
+                })
+        });
 
-        // it('should add start date to card', function (done) {
-        //     var nightmare = Nightmare({ show: true })
+        it('should add start date to card', function (done) {
+            var nightmare = Nightmare({ show: true })
 
-        //     nightmare
-        //         .goto(url)
-        //         .wait('.card-edit-btn')
-        //         .evaluate(()=>{
-        //             var elements = document.querySelectorAll('.card-edit-btn');
-        //             elements[0].click();
-        //         })
-        //         .wait('.date-picker')
-        //         .evaluate(function () {
-        //             var elements = document.querySelectorAll('.date-picker');
-        //             elements[0].value = ""; // clear description
-        //         })
-        //         .type('.date-picker', '01/01/2001')
-        //         .type('body', '\u000d') // enter key
-        //         .evaluate(()=>{
-        //             var elements = document.querySelectorAll('.card-edit-btn');
-        //             elements[0].click();
-        //         })
-        //         .then(function () {
-        //             nightmare
-        //                 .evaluate(()=>{
-        //                     var elements = document.querySelectorAll('.card-edit-btn');
-        //                     elements[0].click();
-        //                 })
-        //                 .wait('.date-picker')
-        //                 .evaluate(function () {
-        //                     var elements = document.querySelectorAll('.date-picker');
-        //                     return elements[0].value;
-        //                 })
-        //                 .then(function (value) {
-        //                     expect(value).to.equal("2001-01-01");
-        //                     done();
-        //                 })
-        //         })
-        // });
+            nightmare
+                .goto(url)
+                .wait('.card-edit-btn')
+                .evaluate(()=>{
+                    var elements = document.querySelectorAll('.card-edit-btn');
+                    elements[0].click();
+                })
+                .wait('.date-picker')
+                .evaluate(function () {
+                    var elements = document.querySelectorAll('.date-picker');
+                    elements[0].value = ""; // clear description
+                })
+                .type('.date-picker', '01/01/2001')
+                .type('body', '\u000d') // enter key
+                .evaluate(()=>{
+                    var elements = document.querySelectorAll('.card-edit-btn');
+                    elements[0].click();
+                })
+                .then(function () {
+                    nightmare
+                        .evaluate(()=>{
+                            var elements = document.querySelectorAll('.card-edit-btn');
+                            elements[0].click();
+                        })
+                        .wait('.date-picker')
+                        .evaluate(function () {
+                            var elements = document.querySelectorAll('.date-picker');
+                            return elements[0].value;
+                        })
+                        .then(function (value) {
+                            expect(value).to.equal("2001-01-01");
+                            done();
+                        })
+                })
+        });
 
 
         it('should mark card as done', function (done) {
